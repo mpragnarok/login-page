25b8b37b2485bba90a3d1e4a9a992ca3eab31248
unknown <mp922352612@gmail.com>
2019-09-01 08:16:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Login page
+
+This is a **Login page** demo which built up from [Alphacamp](https://tw.alphacamp.co/) semester 3 assignment.
+
+![demo](./public/img/demo.png)
+
+
+
+## Installing
+
+### Git bash
+
+#### git clone
+
+`git clone` to clone the project
+
+```markdown
+git clone https://github.com/mpragnarok/login-page.git
+```
+
+### Command line
+
+#### npm install
+
+Install all the packages in the project.
+
+#### npm run dev
+
+Start the server with **nodemon** command.
+
+#### npm run start
+
+Start the server with node command.
+
+#### CTRL + C twice
+
+Stop the server
+
+### Browser
+
+Head to [http://localhost:3000](http://localhost:3000) to experience the login page demo.
+
+## Feature
+
+​	Login page can remember what user have been in put in email field, and react when email address or password is not correct . 
+
+Front-end part is written in **JavaScript**, **bootstrap** and **HTML**, back-end part is written in **Node.js** and **Express.js**.
+
+## packages
+
+#### dependencies
+
+1. express
+2. express-handlebars
+3. body-parser
+
+#### devDependency
+
+1. nodemon
---
+++ app.js
@@ -0,0 +1,51 @@
+// require modules and port setting
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const verified = require('./login.js')
+const port = process.env.PORT || 3000
+const hbs = exphbs.create({
+  extname: '.hbs',
+  defaultLayout: 'main'
+})
+
+// setup handlebars engine and file extension
+
+app.engine(hbs.extname, hbs.engine, hbs.defaultLayout)
+app.set('view engine', hbs.extname)
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// route setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', { message: '' })
+})
+
+app.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const result = verified(email, password)
+
+  if (result !== undefined) {
+    res.render('index', {
+      user: result.firstName,
+    })
+  } else {
+    res.render('login', {
+      email,
+      message: 'email or password is incorrect.'
+    })
+  }
+})
+
+// set listener to Express server
+
+app.listen(port, () => {
+  console.log('Server is started')
+})
